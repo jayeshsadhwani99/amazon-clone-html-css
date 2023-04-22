2ab1ebee9366c434c9737a7d46c283f0ab9729d4
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-04-22 16:43:32 +0530

    footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
+function backToTop() {
+  window.scrollTo({ top: 0 });
+}
 
+// Made by and the copyright
+const copyright = document.getElementById("copyright");
+copyright.innerHTML = `&copy; Jayesh Sadhwani ${new Date().getFullYear()}`;
